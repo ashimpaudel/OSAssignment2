8e2114c06dc8cb2caf5d0db9d1f8da18c6e02568
oscreader <oscreader@OSC>
2018-02-11 18:25:57 -0700

    recursive implementation

--- proj2.c
+++ proj2.c
@@ -14,16 +14,47 @@ int simple_init(void)
 	// we obtain this by looping over the current process, going each level up of the tree till we hit null
 	// the main task to figure out here is which process to halt when
 	// each task_struct has struct *next_task, *prev_task;
-		printk("Our main Task %s (pid = %d)\n",task->comm, task_pid_nr(task));
-		while(task->prev_task != NULL){
-			task = task->prev_task;
-			printk("child task of above task %s (pid = %d)\n",task->comm, task_pid_nr(task));
-		}
+		
+		printk("%s[%d]\n", task->comm, task->pid);
+        printAnecstors(task
+		struct task_struct *my_parent = task->parent;
+		printk("Parent task of main Task %s (pid = %d)\n",my_parent->comm, my_parent->pid);
 	
 	}
 	printk(KERN_INFO "Loading Module\n");
 	return 0;
 }
+
+
+bool printAncestors(struct task_struct *root, int target)
+{
+  /* base cases */
+  if (root == NULL)
+     return false;
+ 
+  if (root->pid == target)
+     return true;
+ 
+  /* If target is present in either l*/
+  for_eah_process(task)
+  {
+    if(printAncestors(task->children, int(pid)){
+        printk("ancestor process %s[%d]\n", task->comm, task->pid);;
+        return true;
+    }
+  }
+ 
+  /* Else return false */
+  return false;
+}
+
+
+
+
+
+
+
+
 /* This function is called when the module is removed. */
 void simple_exit(void)
 {
